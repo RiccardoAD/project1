21597973255ced86ecd146e27af1e79551290ca1
RiccardoAD <rick_95@libero.it>
2024-03-12 17:17:39 +0100

    revision

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,4 +1,4 @@
-
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container'
@@ -10,28 +10,28 @@ import items from '../data/scifi.json'
      let AllTheBooks= function() { 
      return (
     <Container fluid>
-        <Row  >
+        <Row className='gy-3' >
            
                {
                 // allininterno metto tutto quello che devo moltiplicare
-               items.map((libri)=>{
+               items.map((libri,index)=>{
 
 
 
                    return (
                     
-                       <Col>
-                            <Card style={{ width: "14rem" }}>
-                              <Card.Img variant="top" src={libri.img} style={{objectFit: 'contain' }}     />
+                       <Col xs={6} md={4} lg={3} >
+                            <Card    key={index} >
+                              <Card.Img variant="top" src={libri.img} style={{objectFit: 'cover', height:"50%",width:"100%" }}     />
                               <Card.Body>
-                                <Card.Title>{libri.title}</Card.Title>
+                                <Card.Title style={{overflow:"hidden", textOverflow:"ellipsis", whiteSpace:"nowrap"}}>{libri.title}</Card.Title>
                                 <Card.Text>
                                   <p>  {libri.category}   </p>
                                 </Card.Text>
                                 <ListGroup className="list-group-flush">
-                                <ListGroup.Item><p> {libri.price} €  price</p></ListGroup.Item>
+                                <ListGroup.Item><p> {libri.price}  <strong> €  price</strong></p></ListGroup.Item>
                               </ListGroup>
-                               
+                              <Button variant="primary"> compra </Button>
                               </Card.Body>
                             </Card>
     
